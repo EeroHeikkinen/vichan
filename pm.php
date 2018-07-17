4aaa40e977461192989eb8bd18a4b67a68d61423
Eero Heikkinen <eero@Eeros-MacBook-Pro.local>
2018-07-18 02:00:04 +0300

    Fix relative urls to boards from Anon PM page

--- pm.php
+++ pm.php
@@ -16,7 +16,7 @@ function mod_page($title, $template, $args, $subtitle = false) {
 		'hide_dashboard_link' => $template == 'mod/dashboard.html',
 		'title' => $title,
 		'subtitle' => $subtitle,
-		'boardlist' => createBoardlist($mod),
+		'boardlist' => createBoardlist(false),
 		'body' => Element($template,
 				array_merge(
 					array('config' => $config, 'mod' => $mod), 
